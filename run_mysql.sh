e39732b9dac0df7c22e84e1f9ead7859af023be5
Gustavo Andrade <gustavoandrade82@gmail.com>
2018-02-25 10:10:17 -0300

    Removing unused files

--- run_mysql.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker run \
--e MYSQL_ROOT_PASSWORD=secret \
--e MYSQL_DATABASE=twitter-analyzer \
--e MYSQL_USER=twitter \
--e MYSQL_PASSWORD=twitter \
--p 3306:3306 \
--d mysql
--- run_neo4j.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker run \
--d --publish=7474:7474 --publish=7687:7687 \
-neo4j
