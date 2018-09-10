7fc985606b9607d83d44e7d78f34e03d1e2cb737
Gustavo Andrade <gustavoandrade82@gmail.com>
2018-09-10 20:25:53 -0300

    Adding new query methods

--- twitter-service/src/main/java/io/github/gandrade/analyzer/twitter/repository/ProfileRepository.java
+++ twitter-service/src/main/java/io/github/gandrade/analyzer/twitter/repository/ProfileRepository.java
@@ -3,8 +3,16 @@ package io.github.gandrade.analyzer.twitter.repository;
 import io.github.gandrade.analyzer.twitter.domain.Profile;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends Neo4jRepository<Profile, Long> {
 
+    Optional<Profile> findByScreenName(String screenName);
+    Optional<Profile> findByName(String name);
+
+    boolean existsByScreenName(String screenName);
+    boolean existsByName(String name);
 }
